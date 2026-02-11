9b70bf799eea8676277fa1cd189a062baecdfd04
Aman Kanojiya <aman.knj2006@gmail.com>
2026-02-11 15:01:34 +0530

    Screen Layout

--- src/components/app/app-shell.tsx
+++ src/components/app/app-shell.tsx
@@ -5,16 +5,17 @@ import { cn } from "@/lib/utils";
 
 export function AppShell({ children, className }: { children: ReactNode; className?: string }) {
     return (
-        <div className="w-full max-w-sm mx-auto h-[812px] max-h-[90vh] bg-neutral-800 rounded-[48px] shadow-2xl overflow-hidden border-[10px] border-black flex flex-col relative group">
-            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-36 h-8 bg-black rounded-b-2xl z-20 flex items-center justify-center">
-                <div className="w-16 h-1.5 bg-gray-700 rounded-full"></div>
-            </div>
+        <div className="w-full max-w-[400px] mx-auto h-[860px] max-h-[90vh] bg-black rounded-[42px] shadow-2xl overflow-hidden border-[8px] border-neutral-900 flex flex-col relative group">
+            {/* Samsung S25 Punch-hole Camera */}
+            <div className="absolute top-3 left-1/2 -translate-x-1/2 w-3 h-3 bg-black rounded-full z-30 ring-2 ring-neutral-800"></div>
 
+            {/* Screen Container */}
             <div id="app-shell-container" className={cn("flex-grow flex flex-col overflow-hidden relative bg-card", className)}>
                 {children}
             </div>
 
-            <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-32 h-1.5 bg-neutral-600 rounded-full opacity-80 group-hover:opacity-100 transition-opacity z-20"></div>
+            {/* Samsung-style Bottom Gesture Bar */}
+            <div className="absolute bottom-1.5 left-1/2 -translate-x-1/2 w-28 h-1 bg-neutral-700 rounded-full opacity-90 z-20"></div>
         </div>
     );
 }
